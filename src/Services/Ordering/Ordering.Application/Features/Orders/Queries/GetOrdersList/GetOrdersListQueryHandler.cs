f04e79b84f2636283b23e2d44cdb1e54715587ef
Tahir Akram <tahir.akram@trapezegroup.com>
2021-08-21 18:46:08 +0500

    MediatR - Implemented AutoMapper - To map entity/domain object with dto/vm CQRS - Implemented (GetOrderList, CheckoutOrder) Fluent Validator

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _repository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orders);
+        }
+    }
+}
