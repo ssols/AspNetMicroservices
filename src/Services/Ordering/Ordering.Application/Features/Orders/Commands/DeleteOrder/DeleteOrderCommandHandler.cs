02e6746768e3fc5a529826375a44843277fc0bbb
Tahir Akram <tahir.akram@trapezegroup.com>
2021-08-25 16:53:36 +0500

    Ordering.Application using CQRS, DDD

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository repository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _repository.GetByIdAsync(request.Id);
+
+            if (null == orderToDelete)
+            {
+                _logger.LogInformation("Order not exist on database.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            _mapper.Map(request, orderToDelete, typeof(DeleteOrderCommand), typeof(Order));
+            await _repository.UpdateAsync(orderToDelete);
+
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/IRequestHandle.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public interface IRequestHandle
+    {
+    }
+}
